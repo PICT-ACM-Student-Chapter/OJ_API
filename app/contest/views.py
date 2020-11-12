25a7e1897710825c36def96b3ed838b413441b1e
pnshiralkar <pnshiralkar@gmail.com>
2020-11-12 21:06:15 +0530

    Added security details to contest APIs

--- app/contest/views.py
+++ app/contest/views.py
@@ -1,13 +1,16 @@
 # Create your views here.
+from rest_framework import permissions
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 
 from contest.models import Contest
+from contest.permissions import IsAllowedInContest
 from contest.serializers import UserContestSerializer, ContestSerializer
 from core.models import UserContest
 
 
 class ContestList(ListAPIView):
     serializer_class = UserContestSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         return UserContest.objects.filter(user_id=self.request.user.id)
@@ -16,9 +19,5 @@ class ContestList(ListAPIView):
 class ContestDetails(RetrieveAPIView):
     serializer_class = ContestSerializer
     lookup_url_kwarg = 'id'
-
-    def get_queryset(self):
-        if UserContest.objects.filter(
-                user_id=self.request.user.id,
-                contest_id=self.kwargs['id']).count() > 0:
-            return Contest.objects.all()
+    queryset = Contest.objects.all()
+    permission_classes = [permissions.IsAuthenticated, IsAllowedInContest]
--- app/question/serializers.py
+++ app/question/serializers.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers
-
 from question.models import Question
 
 
