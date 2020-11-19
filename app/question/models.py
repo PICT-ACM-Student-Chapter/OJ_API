ecc4c4c37179544f7944a40e77b86bc9d1bc9ff9
pnshiralkar <pnshiralkar@gmail.com>
2020-11-19 12:01:46 +0530

    Added change filename on upload for test-cases

--- app/question/models.py
+++ app/question/models.py
@@ -3,6 +3,21 @@ from django.db import models
 from contest.models import Contest
 
 
+def upload_file_rename(field_name):
+    allowed_field_names = ['input', 'output']
+    if not field_name or field_name not in allowed_field_names:
+        raise Exception("field_name should be either of " +
+                        str(allowed_field_names))
+
+    def wrapper(instance, filename):
+        que_id = instance.que_id.id
+        contest_id = instance.que_id.contest_id.id
+        return "contest_{}/question_{}/testcases/{}{}.txt". \
+            format(contest_id, que_id, field_name, instance.id)
+
+    return wrapper
+
+
 # Create your models here.
 class Question(models.Model):
     id = models.CharField(max_length=10, primary_key=True)
@@ -15,8 +30,8 @@ class Question(models.Model):
 
 class Testcase(models.Model):
     id = models.IntegerField(primary_key=True)
-    input = models.FileField()
-    output = models.FileField()
+    input = models.FileField(upload_to=upload_file_rename("input"))
+    output = models.FileField(upload_to=upload_file_rename("output"))
     que_id = models.ForeignKey(Question, on_delete=models.CASCADE,
                                related_name="test_cases")
     is_public = models.BooleanField(default=False)
