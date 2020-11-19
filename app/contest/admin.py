ab30439de8a9a10c0004f6c555a174e28640f67b
pnshiralkar <pnshiralkar@gmail.com>
2020-11-20 01:50:01 +0530

    Added django-inlines for managing questions in a contest in Admin Panel

--- app/contest/admin.py
+++ app/contest/admin.py
@@ -1,15 +1,26 @@
-from contest.models import Contest
 from django.contrib import admin
 from django.db import models
 from martor.widgets import AdminMartorWidget
 
+from contest.models import Contest
+
 
 # Register your models here.
+from question.models import Question
+
+
+class QuestionsInline(admin.TabularInline):
+    model = Question.contests.through
+    verbose_name = "Question"
+    verbose_name_plural = "Contest Questions"
+
 
 class ContestModelAdmin(admin.ModelAdmin):
     formfield_overrides = {
         models.TextField: {'widget': AdminMartorWidget},
     }
+    inlines = [QuestionsInline]
+    exclude = ['questions']
 
 
 admin.site.register(Contest, ContestModelAdmin)
