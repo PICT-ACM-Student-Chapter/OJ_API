6ea04a4632684c305eba1be29de88a2827d26705
pnshiralkar <pnshiralkar@gmail.com>
2020-11-19 16:34:18 +0530

    Que-Contest ManyToMany models done

--- app/contest/models.py
+++ app/contest/models.py
@@ -2,9 +2,13 @@ from django.db import models
 
 
 # Create your models here.
+from question.models import Question
+
+
 class Contest(models.Model):
     id = models.CharField(max_length=10, primary_key=True)
     name = models.CharField(max_length=30)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
     instructions = models.TextField()
+    questions = models.ManyToManyField(to=Question, related_name='contests')
--- app/question/models.py
+++ app/question/models.py
@@ -1,21 +1,16 @@
 from django.db import models
 
-from contest.models import Contest
+
+def upload_input_rename(instance, filename):
+    que_id = instance.que_id.id
+    return "question_{}/testcases/input{}.txt". \
+        format(que_id, instance.id)
 
 
-def upload_file_rename(field_name):
-    allowed_field_names = ['input', 'output']
-    if not field_name or field_name not in allowed_field_names:
-        raise Exception("field_name should be either of " +
-                        str(allowed_field_names))
-
-    def wrapper(instance, filename):
-        que_id = instance.que_id.id
-        contest_id = instance.que_id.contest_id.id
-        return "contest_{}/question_{}/testcases/{}{}.txt". \
-            format(contest_id, que_id, field_name, instance.id)
-
-    return wrapper
+def upload_output_rename(instance, filename):
+    que_id = instance.que_id.id
+    return "question_{}/testcases/output{}.txt". \
+        format(que_id, instance.id)
 
 
 # Create your models here.
@@ -23,15 +18,13 @@ class Question(models.Model):
     id = models.CharField(max_length=10, primary_key=True)
     name = models.CharField(max_length=30)
     description = models.TextField()
-    contest_id = models.ForeignKey(Contest, on_delete=models.SET_NULL,
-                                   null=True, related_name="questions")
     score = models.IntegerField()
 
 
 class Testcase(models.Model):
     id = models.IntegerField(primary_key=True)
-    input = models.FileField(upload_to=upload_file_rename("input"))
-    output = models.FileField(upload_to=upload_file_rename("output"))
+    input = models.FileField(upload_to=upload_input_rename)
+    output = models.FileField(upload_to=upload_output_rename)
     que_id = models.ForeignKey(Question, on_delete=models.CASCADE,
                                related_name="test_cases")
     is_public = models.BooleanField(default=False)
