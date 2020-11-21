267c08ed32cc4ea639ffcf41ecec0a536e6fbadc
pnshiralkar <pnshiralkar@gmail.com>
2020-11-21 19:12:06 +0530

    Added django-jet as admin-panel theme

--- app/core/admin.py
+++ app/core/admin.py
@@ -2,5 +2,9 @@
 from core.models import Language, UserContest
 from django.contrib import admin
 
+
+admin.site.site_header = 'PASC OJ Management Portal'
+admin.site.site_title = 'PASC OJ Management Portal'
+
 admin.site.register(Language)
 admin.site.register(UserContest)
