ebb0bbfa537fdf9041a724323ab4f6ba51f1c920
pnshiralkar <pnshiralkar@gmail.com>
2020-11-21 18:15:13 +0530

    Added shortt-names to __str__ of models, added TCs as inlines to Questions

--- app/question/admin.py
+++ app/question/admin.py
@@ -1,15 +1,23 @@
 from django.contrib import admin
 from django.db import models
 from martor.widgets import AdminMartorWidget
+
 from question.models import Question, Testcase
 
 
 # Register your models here.
 
+class TestCasesInline(admin.TabularInline):
+    model = Testcase
+    verbose_name = "Testcase"
+    verbose_name_plural = "Testcases"
+
+
 class QuestionModelAdmin(admin.ModelAdmin):
     formfield_overrides = {
         models.TextField: {'widget': AdminMartorWidget},
     }
+    inlines = [TestCasesInline]
 
 
 # Register your models here.
