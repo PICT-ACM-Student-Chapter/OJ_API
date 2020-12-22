4a5eb452ca1e3c29754acdd376101cf480cff466
pnshiralkar <pnshiralkar@gmail.com>
2020-12-22 13:25:00 +0530

    Static files hosted on S3

--- app/app/settings.py
+++ app/app/settings.py
@@ -161,7 +161,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'https://yt-list.s3.ap-south-1.amazonaws.com/staticfiles/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
