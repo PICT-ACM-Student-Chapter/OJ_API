b307e01f847e8a19607d7b9fac34e08f3cc5e4f9
pnshiralkar <pnshiralkar@gmail.com>
2021-01-03 13:04:22 +0530

    Take DB port from env

--- app/app/settings.py
+++ app/app/settings.py
@@ -91,6 +91,7 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASS'),
+        'PORT': os.environ.get('DB_PORT', 5432),
     }
 }
 
