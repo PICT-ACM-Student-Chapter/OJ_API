06c1c3c47b6d243b9bb7669cc3a1919b0d36ccf7
pnshiralkar <pnshiralkar@gmail.com>
2021-03-04 12:41:02 +0530

    Status priority done ; removed unnecessary TODOs

--- app/contest/permissions.py
+++ app/contest/permissions.py
@@ -6,7 +6,6 @@ from rest_framework import permissions
 from core.models import UserContest
 
 
-# TODO: Check if this can be moved to 'core' app
 class IsAllowedInContest(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return UserContest.objects.filter(user_id=request.user.id,
@@ -14,7 +13,6 @@ class IsAllowedInContest(permissions.BasePermission):
                                           ).count() > 0
 
 
-# TODO: Derive from base IsInTime class
 class IsInTime(permissions.BasePermission):
     message = "Access denied. Reason: outside contest time"
 
