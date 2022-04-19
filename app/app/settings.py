6201cbec5ff1476ba391f69a263a76000b7f9808
Akshat Sharma <akshatsharma2602@gmail.com>
2022-04-19 14:04:04 +0530

    Added env example and changed docker-compose

--- app/app/settings.py
+++ app/app/settings.py
@@ -13,20 +13,20 @@ import os
 from datetime import timedelta
 from pathlib import Path
 
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
-from sentry_sdk.integrations.redis import RedisIntegration
+# import sentry_sdk
+# from sentry_sdk.integrations.django import DjangoIntegration
+# from sentry_sdk.integrations.redis import RedisIntegration
 
-sentry_sdk.init(
-    dsn="https://be0b44a7b1e84ae5a21230d3cfb3b0ed@o504182.ingest.sentry.io"
-        "/5590586",
-    integrations=[DjangoIntegration(), RedisIntegration()],
-    traces_sample_rate=1.0,
+# sentry_sdk.init(
+#     dsn="https://be0b44a7b1e84ae5a21230d3cfb3b0ed@o504182.ingest.sentry.io"
+#         "/5590586",
+#     integrations=[DjangoIntegration(), RedisIntegration()],
+#     traces_sample_rate=1.0,
 
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True
-)
+#     # If you wish to associate users to errors (assuming you are using
+#     # django.contrib.auth) you may enable sending PII data.
+#     send_default_pii=True
+# )
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
